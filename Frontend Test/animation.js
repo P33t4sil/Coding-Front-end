c75fc8001727ce7219f7ca4f5e92be341f7a5db9
P33t4sil <149658932+P33t4sil@users.noreply.github.com>
2023-11-03 11:08:47 +0700

    Add files via upload

---
+++ Frontend Test/animation.js
@@ -0,0 +1,9 @@
+
+function openNav() {
+    document.getElementById('sidemenu').style.width = '250px';
+  }
+  
+  function closeNav() {
+    document.getElementById('sidemenu').style.width = '0';
+  }
+  
