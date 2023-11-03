b1955fa218ebdeaccf0dd264939489fe7f8eea14
P33t4sil <149658932+P33t4sil@users.noreply.github.com>
2023-11-03 11:13:01 +0700

    Delete Frontend Test directory

--- Frontend Test/animation.js
+++
@@ -1,9 +0,0 @@
-
-function openNav() {
-    document.getElementById('sidemenu').style.width = '250px';
-  }
-  
-  function closeNav() {
-    document.getElementById('sidemenu').style.width = '0';
-  }
-  
